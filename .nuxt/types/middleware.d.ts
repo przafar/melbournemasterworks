dcc1a166e4ccd3dd9f6958479cee8427e277faaf
Zafar <su10409@studenti.polito.it>
2025-03-05 20:13:09 +0800

    Добавлен .gitignore

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "/Users/zafarcik.polvongmail.com/Documents/Zafar/Melbournemasterworks/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
